8e3c84b8aecf337a88a1801dae35545d13198938
Kamran <kamran.vat@protonmail.com>
2025-07-29 23:13:27 +0200

    fix: mapping data type

--- neuro_ngram.py
+++ neuro_ngram.py
@@ -5,7 +5,7 @@ import numpy as np
 import ast
 from itertools import product
 
-from bpe import load_corpus, create_str_to_int_map, create_int_to_str_map, encode, decode
+from bpe import load_corpus, preprocess_corpus, create_str_to_int_map, create_int_to_str_map, encode, decode
 
 class NeuroNgram(nn.Module):
     def __init__(self, vocab, n=2):
@@ -16,17 +16,24 @@ class NeuroNgram(nn.Module):
         self.embedding = nn.Embedding(self.vocab_size, self.vocab_size)
 
         # create context mapping
-        cs = self.vocab
-        for i in range(1, self.n):
-            # cs = torch.cartesian_prod(cs, self.vocab)
-            cs = list(product(cs, self.vocab.tolist()))
-            
-            print(len(cs))
+        #cs = self.vocab
+        #for i in range(1, self.n):
+        #    # cs = torch.cartesian_prod(cs, self.vocab)
+        #    cs = list(product(cs, self.vocab.tolist()))
+        #    print(f"cs :3 after {i} iteration(s):", cs[:3])
+        #    print("generated tuple amount", len(cs))
+
+        cs = list(product(self.vocab.tolist(), repeat=self.n))
+        print(f"cs[:3] after {self.n} iteration(s):", cs[:3])
+        print("generated tuple amount", len(cs))
 
         cs = torch.tensor(np.array(cs))
-        print(cs.shape)
-        self.context_mapping = dict(zip(cs.tolist(), np.arange(len(cs))))
-        self.inverse_context_mapping = dict(zip(np.arange(len(cs)), cs.tolist()))
+        print("context mapping shape", cs.shape)
+        #self.context_mapping = dict(zip(cs.tolist(), np.arange(len(cs))))
+        #self.inverse_context_mapping = dict(zip(np.arange(len(cs)), cs.tolist()))
+        self.context_mapping = dict(zip(cs, np.arange(len(cs)))) # NOTE: this maps all possible contexts and floods memory for n>2
+        self.inverse_context_mapping = dict(zip(np.arange(len(cs)), cs))
+
         print("context mapping", self.context_mapping)
         # [TODO] create context mappings for all ns
     
@@ -75,28 +82,30 @@ class NeuroNgram(nn.Module):
 
 def test():
     # load  corpus
-    corpus = load_corpus("./corpora/Shakespeare_clean_train.txt")
+    corpus = load_corpus("./corpora/Shakespeare_clean_train.txt", window_size=1000)
+    corpus = preprocess_corpus(corpus)
     # create model
 
     with open('./data/vocab_full_k250.txt', 'r') as f:
         list_string = f.read()
         vocab = ast.literal_eval(list_string)
 
-    corpus = corpus.lower()
-    print("vocab", vocab)
+    # corpus = corpus.lower()
+    # print("vocab", vocab)
 
     string_to_int = create_str_to_int_map(vocab)
     int_to_string = create_int_to_str_map(vocab)
+    print("string to int", string_to_int)
     encoded_vocab = encode(vocab, string_to_int)
-    # print(f"vocab size {vocab.shape}, encoded shape {len(encoded_vocab)}")
-    print(torch.tensor(encoded_vocab).shape)
-    m = NeuroNgram(vocab=torch.tensor(encoded_vocab), n=3)
+    print(f"vocab size {len(vocab)}, encoded shape {len(encoded_vocab)}")
+    print("vocab shape", torch.tensor(encoded_vocab).shape)
+    m = NeuroNgram(vocab=torch.tensor(encoded_vocab), n=2)
 
     corpus = encode(corpus[:1000], string_to_int)
-    print(corpus[:10])
+    print("corpus :10",corpus[:10])
 
     # test batching
-    x, y = m.get_batch(corpus, 4, context_size=8)
+    x, y = m.get_batch(corpus, 16, context_size=2)
     print(x, y)
 
 test()
